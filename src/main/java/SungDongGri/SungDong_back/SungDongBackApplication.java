ba668de266af563a6fe4dec97b0c0c91a2b7edd9
Jyebin <yebin0322@naver.com>
2023-10-13 21:21:22 +0900

    초기 파일 업로드

---
+++ src/main/java/SungDongGri/SungDong_back/SungDongBackApplication.java
@@ -0,0 +1,13 @@
+package SungDongGri.SungDong_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SungDongBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SungDongBackApplication.class, args);
+	}
+
+}
